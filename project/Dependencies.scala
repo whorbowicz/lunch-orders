e8899bd25fef07d857bafa4c9d2fec90f920ff50
Waldemar Horbowicz <waldemar.horbowicz@gmail.com>
2015-10-29 22:50:29 +0100

    Add scalaz dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,11 @@ import sbt._
 
 object Dependencies {
   val scalatest = "org.scalatest" %% "scalatest" % "2.2.4"
+  val scalaz = "org.scalaz" %% "scalaz-core" % "7.1.4"
 
 
   val default = Seq(
+    scalaz,
     scalatest % Test
   )
 }

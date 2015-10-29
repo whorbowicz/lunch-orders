37d29ac32e0ed6455d5e9cd53247737c86a33fc3
Waldemar Horbowicz <waldemar.horbowicz@gmail.com>
2015-10-30 00:33:20 +0100

    Add scalamock to build

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ lazy val core = (project in file("core"))
   .settings(Settings.default: _*)
   .settings(
     name := "lunch-orders-core",
-    libraryDependencies ++= Dependencies.default)
+    libraryDependencies <++= scalaVersion(Dependencies.default))
 
 lazy val root = (project in file("."))
   .settings(Settings.default: _*)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,12 +1,17 @@
 import sbt._
 
 object Dependencies {
+  val scalaReflect = (scalaVersion: String) => "org.scala-lang" %
+    "scala-reflect" % scalaVersion
+  val scalamock = "org.scalamock" %% "scalamock-scalatest-support" % "3.2"
   val scalatest = "org.scalatest" %% "scalatest" % "2.2.4"
   val scalaz = "org.scalaz" %% "scalaz-core" % "7.1.4"
 
 
-  val default = Seq(
+  val default = (scalaVersion: String) => Seq(
     scalaz,
-    scalatest % Test
+    scalatest % Test,
+    scalamock % Test,
+    scalaReflect(scalaVersion) % Test
   )
 }

9a1291fc0d9949c8312a98e8d59ff7027f460988
Waldemar Horbowicz <waldemar.horbowicz@gmail.com>
2015-10-30 23:46:38 +0100

    Define return type

--- core/src/main/scala/com/horbowicz/lunch/orders/domain/order/OpenOrderHandler.scala
+++ core/src/main/scala/com/horbowicz/lunch/orders/domain/order/OpenOrderHandler.scala
@@ -7,14 +7,15 @@ import scalaz._
 
 class OpenOrderHandler[Id](idProvider: IdProvider[Id])
 {
+  type Response = ImpossibleDeliveryTime.type \/ Id
+
   def handle(
     command: OpenOrder,
-    responseCallback: ImpossibleDeliveryTime.type \/ Id => Unit
+    responseCallback: Response => Unit
   ): Unit =
     responseCallback(
       if (command.expectedDeliveryTime.isAfter(command.orderingTime))
         \/-(idProvider.get())
       else
         -\/(ImpossibleDeliveryTime))
-
 }
